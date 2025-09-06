9b32ffaec8c4c3ebd918cd0be51600fd52633a28
Nanavathula Madhulika <nanavathulamadhulika@gmail.com>
2025-09-06 14:01:37 +0530

    Update LinkedListDemo.java

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -22,7 +22,20 @@ public class LinkedListDemo {
         }
         System.out.println("null");
     }
-
+    public void delete(int key) {
+        if (head == null) return;
+        if (head.data == key) {
+            head = head.next;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null && temp.next.data != key) {
+            temp = temp.next;
+        }
+        if (temp.next != null) {
+            temp.next = temp.next.next;
+        }
+    }
     public static void main(String[] args) {
         LinkedListDemo list = new LinkedListDemo();
         list.insertAtBeginning(10);
@@ -30,5 +43,10 @@ public class LinkedListDemo {
         list.insertAtBeginning(30);
         System.out.println("List after insertion at beginning:");
         list.display();
+
+        list.delete(20);
+        System.out.println("List after deleting 20:");
+        list.display();
+
     }
 }
